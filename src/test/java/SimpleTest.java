ff2efef474ab4ebe20e66febcb2f716a234ab7b3
Thierry POUTRAIN <thierry.poutrain@gmail.com>
2013-03-05 22:11:14 +0100

    first

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: poutsjr
+ * Date: 05/03/13
+ * Time: 22:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertTrue(true);
+    }
+
+}
